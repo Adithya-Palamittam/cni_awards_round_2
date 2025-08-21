f521058891a09d90d65d241e5ff1a09c4672de5f
Adithya-Palamittam <adithya@3cctech.com>
2025-08-21 11:01:23 +0530

    Deadline date on login screen changed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -119,7 +119,7 @@ const Login = () => {
         </div>
 
         <div className="text-center text-xs md:text-sm text-gray-600 mt-10">
-              You have till 11:59 pm on Sunday, 24th August  to vote.
+              You have till 11:59 pm on Monday, 25th August  to vote.
         </div>
       </div>
     </div>
